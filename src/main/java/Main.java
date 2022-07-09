f10dbebf85ae66558c0a7ced7695b8438443dbab
nikito <aa@aa.aa>
2022-07-09 11:54:51 +0300

    our first commit add sum function

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int result = sum(10,10);
+        System.out.println(result);
+    }
+
+    static int sum(int a, int b){
+        return  a+b;
+    }
+}
