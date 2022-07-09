25348c622ce6555756ddc8ccba368d3fe228b99b
nikito <aa@aa.aa>
2022-07-09 11:56:18 +0300

    our second commit add division func

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int result = sum(10,10);
+        int resultDivision  = division(20,2);
         System.out.println(result);
+        System.out.println(resultDivision);
     }
 
     static int sum(int a, int b){
         return  a+b;
+    }
+
+    static int division(int a, int b){
+        return  a-b;
     }
 }
